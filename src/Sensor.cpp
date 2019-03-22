5de0e5140c185540c8fcad30e70fe65220ef846c
Corneliusbusch <nicole.meng@hotmail.de>
2019-03-22 19:08:09 +0000

    Adding moving averaging

--- src/Sensor.cpp
+++ src/Sensor.cpp
@@ -1,71 +1,97 @@
-//========================================================
+//==============================================================
+#include "Arduino.h"
 #include "Sensor.h"
-//========================================================
-Sensor::Sensor(int attachedTo, int prev, int thres)
-{
-    pin = attachedTo;
-    prevStates = (int*) realloc(prevStates, prev * sizeof(int));
-    threshold = thres;
+//==============================================================
+
+Sensor::Sensor(int attachedTo, int prev, int thres) {
+  pin = attachedTo;
+  prevStates = new int[prev];
+  threshold = thres;
 }
-//========================================================
-void Sensor::setBaseVal(int newVal)
-{
-    baseVal = newVal;
+
+//==============================================================
+void Sensor::setBaseVal(int newVal){
+  baseVal = newVal;
 }
-//========================================================
-bool Sensor::isTriggeredValue(int newVal)
-{
-    enqueuePrevStates(newVal); // enqueue new value
-    
-    // check how many were triggered from prevStates
-    int counter;
-    int len = sizeof(prevStates) / sizeof(prevStates[0]);
-    if (noReadings < len)
-    {
-        counter = noReadings;
+
+//==============================================================
+bool Sensor::isTriggeredValue(int newVal){
+
+  //Serial.println("start triggered");
+  // enqueue new value
+  enqueuePrevStates(newVal);
+
+
+  // check how many were triggered from prevStates
+  int counter;
+  int len = sizeof(prevStates) / sizeof(prevStates[0]);
+  if(noReadings < len){
+    counter = noReadings;
+  }else{
+    counter = len;
+  }
+
+  int trigg = 0;
+  for(int i = 0; i < counter; i++){
+    int diff = prevStates[i]- baseVal;
+    if(diff > threshold){
+      trigg++;
     }
-    else
-    {
-        counter = len;
-    }
-    int trigg = 0;
-    for (int i = 0; i < counter; i++)
-    {
-        int diff = abs(baseVal - prevStates[i]);
-        if (diff > threshold)
-        {
-            trigg++;
-        }
-    }
-    
-    return (trigg > (counter/3)); // if number triggered larger than not triggered, then accepted
+  }
+
+
+  // if number triggered larger than not triggered, then accepted
+  return(trigg > counter/3);
+
 }
-//========================================================
-void Sensor::enqueuePrevStates(int value)
-{
-    int len = sizeof(prevStates) / sizeof(prevStates[0]);
-    int last;
-    if(noReadings >= len){
+
+//=============================================================
+
+void Sensor::addReading(int val){
+  enqueuePrevStates(val);
+}
+
+//=============================================================
+int Sensor::getSmoothReading(){
+  int len = sizeof(prevStates) / sizeof(prevStates[0]);
+  if(noReadings < len){
+    len = noReadings;
+  }
+  int counter = 0;
+  for(int i = 0; i < counter; i++){
+    counter = counter + prevStates[i];
+  }
+  return(counter/len);
+}
+
+
+//=============================================================
+  void Sensor::enqueuePrevStates(int value){
+
+     int len = sizeof(prevStates) / sizeof(prevStates[0]);
+     //Serial.println(len);
+     int last;
+     if(noReadings >= len){
         int discard = dequeuePrevStates();
         last = len-1;
-    } else{
+     } else{
         last = noReadings;
-    }
-    prevStates[last] = value;
-    noReadings++;
+     }
+     prevStates[last] = value;
+     noReadings++;
+  }
+
+//==============================================================
+int Sensor::dequeuePrevStates(){
+   int len = sizeof(prevStates) / sizeof(prevStates[0]);
+   int first = prevStates[0];
+   for(int i = 1; i < len; i++){
+    prevStates[i-1] = prevStates[i];
+   }
+   prevStates[len-1] = 0;
 }
-//========================================================
-int Sensor::dequeuePrevStates()
-{
-    int len = sizeof(prevStates) / sizeof(prevStates[0]);
-    int first = prevStates[0];
-    for(int i = 1; i < len; i++){
-        prevStates[i-1] = prevStates[i];
-    }
-    prevStates[len-1] = 0;
-}
-//========================================================
-void Sensor::printSensor()
-{
-    Serial.print("pin: " + String(pin) + " base: " + String(baseVal) + " readings: " + String(noReadings));
+
+//==============================================================
+void Sensor::printSensor(){
+  Serial.print("pin: " + String(pin) + " base: " + String(baseVal) + " readings: " + String(noReadings));
 }
--- src/Sensor.h
+++ src/Sensor.h
@@ -1,3 +1,7 @@
+/*
+  Sensor.cpp - Library for Sensor Objects and low-key filtering.
+  Created by Nicole Meng, Mar 10 2019.
+*/
 //========================================================
 #ifndef Sensor_h
 #define Sensor_h
@@ -21,30 +25,42 @@ class Sensor
      printSensor: <#Description#>
      */
     void printSensor();
+
     /**
      setBaseVal: <#Description#>
-
      @param newVal <#newVal description#>
      */
     void setBaseVal(int newVal);
+
     /**
      isTriggeredValue: <#Description#>
-
      @param newVal <#newVal description#>
      @return <#return value description#>
      */
     bool isTriggeredValue(int newVal);
+
+    /**
+     addReading: <#Description#>
+     @param val <#newVal description#>
+     @return <#return value description#>
+     */
+    void addReading(int val);
+    
+    /**
+     getSmoothReading: <#Description#>
+     @return <#return value description#>
+     */
+    int getSmoothReading();
+
     //======================================================
   private:
     /**
      enqueuePrevStates: <#Description#>
-
      @param value <#value description#>
      */
     void enqueuePrevStates(int value);
     /**
      dequeuePrevStates: <#Description#>
-
      @return <#return value description#>
      */
     int dequeuePrevStates();
